a9efb09551176e2a82e7c0457a581bc97202fa88
Cherryjery <104814469+Cherryjery@users.noreply.github.com>
2022-05-05 14:41:36 +0530

    Update Sparrow.java

--- jack/src/main/java/jack/Sparrow.java
+++ jack/src/main/java/jack/Sparrow.java
@@ -13,7 +13,8 @@ public class Sparrow {
 		String log4jConfigFile = System.getProperty("user.dir") + File.separator + "log4j.properties";
 		PropertyConfigurator.configure(log4jConfigFile);
 		logger.info("this is a message....:"+str);
-		logger.warn("warning message...:"+str);
+		logger.warn("Prasanth is looking please be aware "+str);
 		logger.error("error message...:"+str);
+		
 	}
 }
