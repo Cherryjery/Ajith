7c420d11540efcc0e0e9ffd1d56ca984faafcd87
ajithkumar.raja <ajithkumar.raja@DT150138.QICHOAPPL.com>
2022-06-06 11:06:48 +0530

    done

---
+++ labwork/src/main/java/labwork/ExecptionArrayErrorDemo.java
@@ -0,0 +1,17 @@
+package labwork;
+
+public class ExecptionArrayErrorDemo {
+	
+		public static void main(String[] args) {
+			System.out.println("before exception...");
+			try{
+				String s=args[0];
+				Integer.parseInt(s);
+			}catch(ArrayIndexOutOfBoundsException ae) {
+				System.out.println(ae);
+			}finally {
+				System.out.println("finally block code called...");
+			}
+			System.out.println("after exception");
+		}
+	}
