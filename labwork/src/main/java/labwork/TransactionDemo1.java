7fa97e4fa6e9c2dbe64594046ccf3db4a3558b0e
ajithkumar.raja <ajithkumar.raja@DT150138.QICHOAPPL.com>
2022-06-10 16:06:23 +0530

    all done

---
+++ labwork/src/main/java/labwork/TransactionDemo1.java
@@ -0,0 +1,15 @@
+package labwork;
+
+public class TransactionDemo1 implements Cloneable{
+private static TransactionDemo1 transobject ;
+	int amt ;
+	public TransactionDemo1 () {
+		System.out.println("Transaction object is created ajith");
+	}
+ 
+	public static void main(String[] args) {
+		
+
+	}
+
+}
