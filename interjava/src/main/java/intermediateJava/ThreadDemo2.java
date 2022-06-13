66f2527985a127112e7fd148c3c4cd8b7387e944
ajithkumar.raja <ajithkumar.raja@DT150138.QICHOAPPL.com>
2022-06-13 13:00:06 +0530

    cherry

---
+++ interjava/src/main/java/intermediateJava/ThreadDemo2.java
@@ -0,0 +1,28 @@
+package intermediateJava;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadDemo2 {
+
+	public static void main(String[] args) {
+	System.out.println("Sambar");
+	System.out.println("SOUP...");
+	ExecutorService es=Executors.newFixedThreadPool(1);
+	es.execute(()->{met();});
+	
+	es.shutdown();
+	
+	
+	System.out.println("STARTER.....");}
+	
+	public static void met() {
+		try {Thread.sleep(5000);}catch(Exception e) {}
+		System.out.println("MAIN ITEM....");
+	
+
+
+	}
+	
+
+}
