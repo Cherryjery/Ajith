d5536b91e8d88f25beed9d027a5cb858566ec758
elavarasan.p <elavarasan.p@DT150150.QICHOAPPL.com>
2022-06-17 09:45:37 +0530

    done

---
+++ projectscrap/src/main/java/ajith/AdditionObject.java
@@ -0,0 +1,19 @@
+package ajith;
+
+public class AdditionObject {
+	public void  add () {
+		 
+	
+	int i= 10;
+	int j = 20;
+	int result = i+j ;
+	System.out.println(result);
+	}
+	public static void main(String[] args) {
+		AdditionObject m = new AdditionObject ();
+		m.add();
+		
+
+	}
+
+}
